39df75581d5c7c0a5709b6b69ea53c4ae89213aa
João <joaovitorvlf95@gmail.com>
2025-09-02 17:52:10 -0300

    Primeiro commit

---
+++ src/main/java/com/uninove/projeto/projetojoao/ProjetojoaoApplication.java
@@ -0,0 +1,13 @@
+package com.uninove.projeto.projetojoao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojoaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojoaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uninove/projeto/projetojoao/ProjetojoaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninove.projeto.projetojoao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojoaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
